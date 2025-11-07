e43385a5cb8ebd16072f5006a08c894fde7b7863
aleksz <alekszurowski@gmail.com>
2025-11-07 20:04:17 +0100

    add new ScoreCounter animation

--- components/ScoreCounter.tsx
+++ components/ScoreCounter.tsx
@@ -1,5 +1,5 @@
 import { TextInput, TextStyle } from "react-native"
-import Animated, { SharedValue, useAnimatedProps } from "react-native-reanimated"
+import Animated, { interpolateColor, SharedValue, useAnimatedProps, useAnimatedReaction, useAnimatedStyle, useSharedValue, withSequence, withSpring, withTiming } from "react-native-reanimated"
 
 const AnimatedTextInput = Animated.createAnimatedComponent(TextInput);
 
@@ -8,7 +8,56 @@ interface ScoreCounterProps{
     style: TextStyle;
 }
 
+const normalColor = "#ffffff";
+const goodColor = "#7596dd";
+const badColor = "#ff0000";
+
 export default function ScoreCounter({score, style}: ScoreCounterProps){
+
+    const scale = useSharedValue(1);
+    const colorProgress = useSharedValue(0);
+
+    useAnimatedReaction(
+        () => {
+            'worklet'
+            return score.value;
+        },
+        (currentScore, previousScore) => {
+            'worklet'
+            
+            if (currentScore > (previousScore ?? 0)) {
+                scale.value = withSequence(
+                    withTiming(1.5, { duration: 100 }),
+                    withSpring(1)
+                )
+                
+                colorProgress.value = withSequence(
+                    withTiming(1, { duration: 100 }),
+                    withTiming(0, { duration: 400 })
+                )
+            } else if (currentScore === 0 && (previousScore ?? 0) > 0) {
+                colorProgress.value = withSequence(
+                    withTiming(2, { duration: 100 }),
+                    withTiming(0, { duration: 400 })
+                )
+            }
+        },
+        [score]
+    );
+
+    const animatedStyle = useAnimatedStyle(() => {
+        'worklet';
+        const animatedColor = interpolateColor(
+            colorProgress.value,
+            [0, 1, 2], 
+            [normalColor, goodColor, badColor]
+        )
+
+        return {
+            transform: [{ scale: scale.value }],
+            color: animatedColor,
+        }
+    });
     
     const animatedProps = useAnimatedProps(() => {
         return { 
@@ -18,6 +67,6 @@ export default function ScoreCounter({score, style}: ScoreCounterProps){
     });
 
     return(
-        <AnimatedTextInput editable={false} style={style} animatedProps={animatedProps}/>
+        <AnimatedTextInput editable={false} style={[style, animatedStyle]} animatedProps={animatedProps}/>
     )
 }
