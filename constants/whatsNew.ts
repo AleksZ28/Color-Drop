2309735e0ddb637478c4cf1b8401e7b374432577
AleksZ <alekszurowski@gmail.com>
2026-01-09 19:20:40 +0100

    rename images due to build errors

--- constants/whatsNew.ts
+++ constants/whatsNew.ts
@@ -5,18 +5,18 @@ export const whatsNewData: WhatsNewItem[] = [
         id: '1',
         title: 'Krople bomby 💣',
         description: 'Od teraz powyżej 200 punktów pojawiają się krople-bomby, które musisz neutralizować! Przewiń w bok, aby dowiedzieć się więcej.',
-        image: require('@/assets/images/whatsnew_bomb0.png'),
+        image: require('@/assets/images/whatsnew-bomb0.png'),
     },
     {
         id: '2',
         title: 'Bomba standardowa',
         description: 'W przypadku bomby o standardowym kolorze, unikaj jej koloru.',
-        image: require('@/assets/images/whatsnew_bomb1.png'),
+        image: require('@/assets/images/whatsnew-bomb1.png'),
     },
     {
         id: '3',
         title: 'Bomba zmieszana',
         description: 'W przypadku bomby o zmieszanych kolorach, unikaj kolorów, z których została zmieszana.',
-        image: require('@/assets/images/whatsnew_bomb2.png'),
+        image: require('@/assets/images/whatsnew-bomb2.png'),
     }
 ];
